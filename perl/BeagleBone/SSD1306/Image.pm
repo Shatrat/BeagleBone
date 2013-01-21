a164a1b1db480b81f5351af00f5e1054dcee518d
Jonathan Williams <shatrat@arm.(none)>
2013-01-21 15:30:35 -0600

    moved imageToBuffer sub to a new module Image.pm

---
+++ perl/BeagleBone/SSD1306/Image.pm
@@ -0,0 +1,66 @@
+package BeagleBone::SSD1306::Image;
+use 5.14.1;
+use warnings;
+use parent 'BeagleBone::SSD1306';
+use Imager;
+
+=head1 NAME
+
+BeagleBone::SSD1306::Image
+
+=head1 SYNOPSIS
+
+Provides subroutines to manipulate and display Imager objects that are useful for SSD1306 monochrome displays
+
+This assumes a grayscale image of the same size as the monochrome display
+	Imager->new(xsize => 128, ysize => 64, channels=>1, bits=>1);
+
+
+=cut
+
+=head2 imageToBuffer
+
+Accept an Imager image and return an array of 8 bit numbers
+Was written to work with grayscale Imager objects.
+Should work with 128x64, 128x32, and possibly other SSD1306 variations (128x16? 64x64?)
+
+=cut
+
+
+sub imageToBuffer {
+	my $image = shift;
+	my $pixel_color = 0;
+	my @bits = [];
+	my $page = '';
+	my @buffer;
+	my $x = 0;
+	my $y_page = 0;
+	my $y_bit = 0;
+	
+	#loop through pages (height/8) by columns (width). Each page is a vertical row of 8 pixels with LSB at the top.
+	for(0..($image->getheight()/8 - 1)){
+		$y_page = $_;
+		for(0..($image->getwidth()-1)){
+			$x = $_;
+			for(0..7){
+				$y_bit = $_;
+				$pixel_color = $image->getpixel('x' => $x,'y' => $y_page*8 + $y_bit) or die "cannot getpixel $x, $y_page *8 + $y_bit on image ", $image->errstr;
+				if(($pixel_color->rgba())[0]){
+					$bits[$y_bit] = 1;
+				}
+				else{
+					$bits[$y_bit] = 0;
+				}
+			}
+			#take array of bits and convert them to a scalar byte, thusly. (0,0,1,0,0,0,1,0) = (34)
+			$page = unpack( 'C',pack('b8',join('',@bits)));
+			push(@buffer,$page);
+			
+		}
+		
+	}
+	return \@buffer;
+
+}
+
+1;
--- perl/beagle_buell_imager.pl
+++ perl/beagle_buell_imager.pl
@@ -6,20 +6,21 @@
 #Basic sequence (hopefully)
 #init {create image template, check everything to make sure it's working}
 #loop
-#	request serial data
-#	read serial data, generate text
+#	TODO: request serial data
+#	TODO: rx serial data
+#	generate text
 #	draw text onto copy of blank image
 #	traverse image pixels to get binary representation
 #	send binary representation to 1306 chip via perl module
 #	check GPIO to make sure bike is still running (if not init 0)
-#	usleep 200ms
+#	sleep just so we don't completely peg the little ARM cpu
 #
 
 use strict;
 use Data::Dumper;
 use Imager;
 use Time::HiRes;
-use BeagleBone::SSD1306;
+use BeagleBone::SSD1306::Image;
 
 #set up some variables for use in Imager
 my $font_filename = 'UbuntuMono-R.ttf';
@@ -42,63 +43,64 @@ my $black_color = Imager::Color->new(0,255,0,0);
 $blank_image->box(filled => 1, color => $white_color);
 $blank_image->box(xmin => 1, ymin => 1, xmax => 126, ymax => 62, filled => 1, color => $black_color);
 
-my $OLED_image;
-my %text_hash;
-my $start;
-my $elapsed;
-my $buffer_ref;
-
-
 
 #loop through fetching info and updating screen
 while(1){
 	#get timestamp for ghetto performance monitoring
-	$start = Time::HiRes::time;
+	my $start = Time::HiRes::time;
 
-	&assignTexts();
+	#&assignTextsNotRef();
+	my $text_hr = &assignTexts();
 	#make a copy of the empty image to be written on and then displayed
-	$OLED_image = $blank_image->copy();
-	&drawImage();
-	#&writeToFile("bb$_.bmp",$OLED_image);
-	#&imageToStdout();
+	my $OLED_image = $blank_image->copy();
+	#&drawImageNotref();
+	&drawImage($text_hr,$OLED_image);
 
-	$buffer_ref = &imageToBuffer($OLED_image);	
+	$OLED_image->flip(dir=>"vh");
+	my $buffer_ref = BeagleBone::SSD1306::Image::imageToBuffer($OLED_image);	
 	
 	#now write image to screen from the buffer created by imageToBuffer
 	my $r = $lcd->writeBulk($buffer_ref);
 	
-	$elapsed = Time::HiRes::time - $start;
-	print "printed " . @$buffer_ref . " bytes to screen returning [". $r ."] in ". sprintf("%.3f",$elapsed) ." seconds\n";
+	my $elapsed = Time::HiRes::time - $start;
+	print "printed [". $r ."] bytes to screen in ". sprintf("%.3f",$elapsed) ." seconds\n";
 	sleep(1);
 }
 
-
+#for testing purposes, writes image to a file.  File type is determined by Imager using the file extension, try 'image.bmp' for example
 sub writeToFile{
-	my ($file, $image) = @_;
-	$OLED_image->write(file=>$file)	or die 'Cannot save $file: ', $OLED_image->errstr;
+	my ($filename, $image) = @_;
+	$image->write(file=>$filename)	or die 'Cannot save $filename: ', $image->errstr;
 }
 
-	
+#returns ref to a hash full of 'key' => [string, x, y, fontsize]
+#strings are pulled from various inputs to the BeagleBone
 sub assignTexts {
 
 	my $temp_fahrenheit = sprintf("%.1f",&getTMP36_temp()) . "f";
 	my $time = 	(localtime)[2] % 12 .":". sprintf("%01d",(localtime)[1]);
-	%text_hash = (
+	my $gear = sprintf("%1d",$temp_fahrenheit % 6);
+	my %result_hash = (
 	#expected format is 'key' => [string, x, y, fontsize]
 	'line1' => ["Air     77f",2,12,14],
 	'line2' => ["Oil    212f",2,24,14],
 	'line3' => ["Beagle $temp_fahrenheit",2,36,14],
 	'line4' => ["Time   $time",2,48,14],
 	'line5' => ["Volts  13.8",2,60,14],
-	'gear' => ["5",88,52,72],
+	'gear' => ["$gear",88,52,72],
 	);
+	return \%result_hash;
 
 }
  
+#accepts a ref to a hash full of 'key' => [string, x, y, fontsize]
+#and an image onto which we will draw them
 sub drawImage {           
-	foreach my $line (keys %text_hash) {
-		my ($string,$x,$y,$font_size)= @{$text_hash{$line}};
-		$OLED_image->string(x => $x, y => $y,
+	my ($text_hr, $image) = @_;
+	
+	foreach my $line (keys %$text_hr) {
+		my ($string,$x,$y,$font_size)= @{$text_hr->{$line}};
+		$image->string(x => $x, y => $y,
 			string => $string,
 			font => $font,
 			size => $font_size,
@@ -106,45 +108,11 @@ sub drawImage {
 			color => $white_color);
 	}
 } 
-
-
-sub imageToStdout{        
-##iterate through the pixels, just to check speed
-	my $x = 0;
-	my $y = 0;
-	my $pixel_color = 0;
-	my $pixel_bit = 0;
-	my @pixel_channels = (0,0,0,0);
-	
-	for (0..63){
-		$y = $_;
-		for (0..127){
-			$x = $_;
-			#get 4 channel color of pixel at x,y
-			$pixel_color = $OLED_image->getpixel(x => $x, y => $y);
-			@pixel_channels = $pixel_color->rgba();#rgba() is a bit of a misnomer in a grayscale image...however there's no ga__() function
-			#desired image is stored in first channel
-			#$pixel_bit = $pixel_channels[0];
-			#increase value of first channel for testing so image is legible
-			if($pixel_channels[0]){
-				$pixel_bit = 1;
-			}
-			else{
-				$pixel_bit = 0;
-			}
-		
-			#console print pixel at x,y
-			#print "$pixel_bit";
-			#if($x==127){
-			#	print "\n";
-			#}
-		}
-	
-	}
-}              
+         
 	
 #accept an Imager image and return an array of 8 bit numbers
-sub imageToBuffer(){
+#should work with 128x64, 128x32, and possibly other SSD1306 variations (128x16? 64x64?)
+sub imageToBuffer {
 
 	my $image = shift;
 	my $pixel_color = 0;
@@ -155,10 +123,10 @@ sub imageToBuffer(){
 	my $y_page = 0;
 	my $y_bit = 0;
 	
-	#loop through 8 pages by 128 columns. Each page is a vertical row of 8 pixels with LSB at the top.
-	for(0..7){
+	#loop through pages (height/8) by columns (width). Each page is a vertical row of 8 pixels with LSB at the top.
+	for(0..($image->getheight()/8 - 1)){
 		$y_page = $_;
-		for(0..127){
+		for(0..($image->getwidth()-1)){
 			$x = $_;
 			for(0..7){
 				$y_bit = $_;
@@ -172,7 +140,6 @@ sub imageToBuffer(){
 			}
 			#take array of bits and convert them to a scalar byte, thusly. (0,0,1,0,0,0,1,0) = (34)
 			$page = unpack( 'C',pack('b8',join('',@bits)));
-	#		print Dumper($page);
 			push(@buffer,$page);
 			
 		}
@@ -193,7 +160,5 @@ sub getTMP36_temp{
 	#(millivolts - 500) /10 returns degrees C for TMP36 chip
 	#Then convert from C to F
 	return ((($digital_reading/4095)*1800-500)/10)*9/5 + 32;
-	
-	
 	
 }
