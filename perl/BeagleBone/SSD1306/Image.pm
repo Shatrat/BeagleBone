0461e367e0eea50b2e68deb4dfa45796c3f6e6fd
Jonathan Williams <shatrat@arm.(none)>
2013-01-23 07:52:08 -0600

    made some efficiency changes to Image.pm

--- perl/BeagleBone/SSD1306/Image.pm
+++ perl/BeagleBone/SSD1306/Image.pm
@@ -3,6 +3,7 @@ use 5.14.1;
 use warnings;
 use parent 'BeagleBone::SSD1306';
 use Imager;
+use Data::Dumper;
 
 =head1 NAME
 
@@ -24,6 +25,9 @@ Accept an Imager image and return an array of 8 bit numbers
 Was written to work with grayscale Imager objects.
 Should work with 128x64, 128x32, and possibly other SSD1306 variations (128x16? 64x64?)
 
+rev1 averaged .682 seconds per frame
+rev2 averaged .681 spf, this is with calling getheight() and getwidth() up front
+
 =cut
 
 
@@ -32,23 +36,23 @@ sub imageToBuffer {
 	my $pixel_color = 0;
 	my @bits = [0,0,0,0,0,0,0,0];
 	my $page = '';
-	my @buffer;
+	my @buffer = (0) x 1024;
 	my $x = 0;
 	my $y_page = 0;
 	my $y_bit = 0;
+	my $width = $image->getwidth();
+	my $height = $image->getheight();
 	
 	#loop through pages (height/8) by columns (width). Each page is a vertical row of 8 pixels with LSB at the top.
-	for(0..($image->getheight()/8 - 1)){
+	for(0..($height/8 - 1)){
 		$y_page = $_;
-		for(0..($image->getwidth()-1)){
+		for(0..($width-1)){
 			$x = $_;
 			for(0..7){
 				$y_bit = $_;
 				$pixel_color = $image->getpixel('x' => $x,'y' => $y_page*8 + $y_bit) 
 					or die "cannot getpixel $x, $y_page *8 + $y_bit on image ", $image->errstr;
-				
-				#TODO see if there is a way to read the first channel of color index directly
-				#without calling the rgba function and incurring call stack overhead
+
 				if(($pixel_color->rgba())[0]){
 					$bits[$y_bit] = 1;
 				}
@@ -58,10 +62,7 @@ sub imageToBuffer {
 			}
 			#take array of bits and convert them to a scalar byte, thusly. (0,0,1,0,0,0,1,0) = (34)
 			$page = unpack( 'C',pack('b8',join('',@bits)));
-			push(@buffer,$page);
-			#TODO instead of using push, rewrite to use $buffer[$y_page*$image->getwidth() + $x] = $page;
-			#because writing to a fixed size array is faster than pushing to one
-			#also call getwidth and getheight once and store the value
+			$buffer[$y_page*$width + $x] = $page;
 		}
 		
 	}
